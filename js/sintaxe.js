bc8c7e38ba163f102d2aafc603fdc5f8543bfc37
pedro.rodrigues <pedroh.mix@gmail.com>
2021-02-02 17:08:33 -0300

    padronizando projeto

---
+++ js/sintaxe.js
@@ -0,0 +1,12 @@
+var idade = 24;
+
+console.log(idade)
+
+// comentário de uma linha
+
+/* isto é um comentário longo
+   de múltiplas linhas.
+ */
+
+/* Você não pode, porém, /* aninhar comentários */
+
---
+++ js/tiposJS.js
@@ -0,0 +1,5 @@
+var terceiro = document.getElementById('terceiro');
+
+terceiro.onclick = function() {
+    document.getElementById("h3Demo").innerHTML = "JAVASCRIPT EXTERNO"
+}
