2921a09668a019a174d0dab0480b65ffc8c60d06
pedro.rodrigues <pedroh.mix@gmail.com>
2021-02-02 18:20:56 -0300

    adicionandoscripts

--- js/sintaxe.js
+++ js/sintaxe.js
@@ -1,3 +1,4 @@
+
 var idade = 24;
 
 console.log(idade)
@@ -10,3 +11,23 @@ console.log(idade)
 
 /* Você não pode, porém, /* aninhar comentários */
 
+var Idade_Pai = idade + 15;
+
+console.log(Idade_Pai)
+
+var idades = [25, 3, 4, 5];
+
+console.log(idades)
+
+console.log(idades[0])
+
+
+var listaIdades = {idadePai: 45, idadeFilho:20, idadeMae:44}
+
+console.log(listaIdades)
+
+listaIdades.idadeFilho = 100
+
+console.log(listaIdades.idadePai)
+console.log(listaIdades.idadeFilho)
+console.log(listaIdades.idadeMae)
