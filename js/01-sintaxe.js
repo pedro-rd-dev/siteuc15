773ff2417d8b1d10be7a822c8e892add7897febc
pedro-rd <pedro.rd.dev@gmail.com>
2021-02-03 21:43:13 -0300

    inserindo condicionantes

--- js/01-sintaxe.js
+++ js/01-sintaxe.js
@@ -1,23 +1,68 @@
 //ATRIBUINDO VALOR À VARIAVEL
 //TIPO NOME = VALOR
-var idade = 24;
 
-console.log("idade: " + idade)
+
+var primeiroNumero = 2;
+var segundoNumero = 4;
+/*
+var resultado;
+
+resultado = primeiroNumero + segundoNumero;
+console.log("SOMA: " + resultado)
+
+resultado = primeiroNumero - segundoNumero;
+console.log("SUBTRACAO: " + resultado)
+
+resultado = primeiroNumero / segundoNumero;
+console.log("DIVISAO: " + resultado)
+
+resultado = primeiroNumero * segundoNumero;
+console.log("MULTIPLICACAO: " + resultado)
+
+var media = (primeiroNumero + segundoNumero )/2;
+
+
+var idade = 33;
+var nome = "pedro"
+
+console.log("A idade de " + nome + " é " + idade + " anos")
+
 
 //ALTERANDO VALOR DE VARIÁVEL
 var Idade_Pai = idade + 15;
 
 console.log("Idade pai: " + Idade_Pai)
 
+*/
 //ARRAYS
-var idades = [25, 3, 4, 5];
 
-console.log("Idades: "+idades)
+/*
+var listaDeNomes = ["ORNINTORRINCO", 45, "murilo"]
 
-console.log("Primeira idade da lista: " + idades[0])
+console.log(listaDeNomes)
 
+listaDeNomes[1] = 100
+console.log(listaDeNomes)
+
+*/
+
+
+var computador = { cor: "preto", modelo: "lenovo", processador: "intel" }
+
+console.log(computador);
+
+console.log(computador.cor);
+console.log(computador.modelo);
+console.log(computador.processador);
+
+
+
+
+
+
+/*
 //LISTAS
-var listaIdades = {idadePai: 45, idadeFilho:20, idadeMae:44}
+var listaIdades = { idadePai: 45, idadeFilho: 20, idadeMae: 44 }
 
 console.log(listaIdades)
 
@@ -26,3 +71,4 @@ listaIdades.idadeFilho = 100
 console.log("Idade pai: " + listaIdades.idadePai)
 console.log("Idade filho: " + listaIdades.idadeFilho)
 console.log("Idade mae: " + listaIdades.idadeMae)
+*/
--- js/02-op_condifionais_IF_ELSE.js
+++ js/02-op_condifionais_IF_ELSE.js
@@ -2,6 +2,9 @@ var idadePai = 40;
 var idadeFilho = 14;
 var idadeMae = 39;
 
+
+
+
 if (idadeFilho === 15) {
     console.log(idadeFilho + " É IGUAL A 15 ANOS")
 } else {
@@ -24,4 +27,21 @@ if (idadeFilho < 15) {
     console.log(idadeFilho + " É MENOR QUE 15 ANOS")
 } else {
     console.log(idadeFilho + " É IGUAL OU MAIOR QUE 15 ANOS")
+}
+
+
+if (idadeFilho == idadePai) {
+    console.log("A IDADE DO FILHO IGUAL A IDADE DO PAI")
+}
+
+if (idadeFilho >= idadePai) {
+    console.log("A IDADE DO FILHO MAIOR A IDADE DO PAI")
+}
+
+if (idadeFilho <= idadePai) {
+    console.log("A IDADE DO FILHO MENOR A IDADE DO PAI")
+
+    if (idadeFilho === 14) {
+        console.log("A  IDADE DO FILHOR É 14")
+    }
 }
