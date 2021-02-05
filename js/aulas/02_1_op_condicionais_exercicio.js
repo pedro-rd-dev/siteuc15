e1b9758dc4ab90b7a258db99b48f9021f65a9485
pedro-rd <pedro.rd.dev@gmail.com>
2021-02-04 21:18:10 -0300

    adicionando exercicios

--- js/aulas/02_1_op_condicionais_exercicio.js
+++ js/aulas/02_1_op_condicionais_exercicio.js
@@ -7,3 +7,28 @@
 
 //OBS O CÓDIGO DEVE MOSTRAR AO USUÁRIO O MOTIVO PELO QUAL NÃO PODE SER CADASTRADO
 
+console.clear();
+var candidato = { nome: "Antonio", atestadoDeAntecedendes: true, idade: 54, nacionalidade: "Brasileiro" };
+console.log("Nome do candidato: " + candidato.nome);
+console.log("Status do atestado de antecedentes criminais, sendo válido igual a true e inválido igual a false: " + candidato.atestadoDeAntecedendes);
+console.log("Idade do candidadto: " + candidato.idade);
+console.log("Nacionalidade do candidato: " + candidato.nacionalidade);
+console.log("");
+
+if (candidato.atestadoDeAntecedendes == true) {
+    if (candidato.idade >= 18) {
+        if (candidato.nacionalidade == "Brasileiro" || candidato.nacionalidade == "Argentino")
+            console.log("Esse candidato foi aprovado com sucesso");
+    }
+}
+
+
+if (candidato.atestadoDeAntecedendes != true) {
+    console.log("Candidato rejeitado devido a problemas com atestado de antecedentes criminais.");
+}
+if (candidato.idade < 18) {
+    console.log("Candidato rejeitado por ser menor de idade.");
+}
+if (candidato.nacionalidade != "Brasileiro" && candidato.nacionalidade != "Argentino") {
+    console.log("Candidato rejeitado por questões de nacionalidade incompatível: " + candidato.nacionalidade + ".");
+}
