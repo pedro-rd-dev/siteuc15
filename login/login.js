7cf3784d91703edb3f43f4166ac96a7c4f65b00a
PEDRO <pedroh.mix@gmail.com>
2021-02-11 01:24:10 -0300

    Logando o usuário

--- login/login.js
+++ login/login.js
@@ -1,5 +1,8 @@
 var objetoEmail = document.getElementById("exampleInputEmail1");
+var objetoPassword = document.getElementById("exampleInputPassword1");
 var objetoAlertaEmail = document.getElementById("alertaEmail");
+
+
 
 
 
@@ -11,9 +14,25 @@ function login() {
     if (!retorno) {
         objetoAlertaEmail.innerHTML = "Este não é um email  valido"
     } else {
-
+        logar();
         objetoAlertaEmail.innerHTML = ""
 
     }
 
+}
+
+function logar(){
+    var usuario = JSON.stringify({email:objetoEmail.value, password: objetoPassword.value})
+
+    $.ajax({
+        url: "http://escolarapp2.herokuapp.com/account/login/",
+        contentType: 'application/json',
+        cache: false,
+        method: 'POST',
+        dataType: 'json',
+        data: usuario,
+        success: function(msg){
+            console.log(msg)
+        }
+    });
 }
