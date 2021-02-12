3110ae916c642b9c369f26aab867d67572709b00
pedro.rodrigues <pedroh.mix@gmail.com>
2021-02-12 19:00:37 -0300

    Enviando post de login

--- auth/login/login.js
+++ auth/login/login.js
@@ -1,11 +1,12 @@
 var objetoEmail = document.getElementById("exampleInputEmail1");
 var objetoPassword = document.getElementById("exampleInputPassword1");
 var objetoAlertaEmail = document.getElementById("alertaEmail");
+var objetotextoLogando = document.getElementById("textoLogando");
+var objetoTextoLogin = document.getElementById("textoLogin");
 
-
-
-
-
+objetotextoLogando.style.display = 'none';
+objetoEmail.value = "pedroh.mix@gmail.com"
+objetoPassword.value = "123456";
 
 function login() {
 
@@ -14,9 +15,13 @@ function login() {
     if (!retorno) {
         objetoAlertaEmail.innerHTML = "Este não é um email  valido"
     } else {
-        logar();
+
+        objetoTextoLogin.style.display = 'none';
+        objetotextoLogando.style.display = 'inline';
         objetoAlertaEmail.innerHTML = ""
 
+
+        logar();
     }
 
 }
@@ -31,8 +36,38 @@ function logar(){
         method: 'POST',
         dataType: 'json',
         data: usuario,
-        success: function(msg){
-            console.log(msg)
+        success: function(objetoToken){
+            console.log(objetoToken)
+
+            /////////////////////ARMAZENAMENTO//////////////////////////
+            //TRANSFORMANDO O OBJETO EM UM JSON (STRING)
+            JsonToken = JSON.stringify(objetoToken);
+
+            //SALVANDO O TOKEN NO LOCAL STORAGE DO NAVEGADOR
+            window.localStorage.setItem("Token",JsonToken);
+
+            //////////////////////////LEITURA//////////////////////////
+            //RECUPERANDO O JSON (STRING) DO LOCAL STORAGE DO NAVEGADOR
+            var jsonTokenStorage = window.localStorage.getItem("Token");
+            console.log("jsonTokenStorage: " +jsonTokenStorage)
+
+            //TRANSFORMANDO O JSON EM UM OBJETO
+            var objetoTokenStorage = JSON.parse(jsonTokenStorage);
+            console.log(objetoTokenStorage)
+
+            objetotextoLogando.style.display = 'none';
+            objetoTextoLogin.style.display = 'inline';
+
+        },
+        error: function (error){
+            var erros = JSON.parse(error.responseText)
+
+            console.log(erros.non_field_errors[0])
+
+            objetoAlertaEmail.innerHTML  =erros.non_field_errors[0]
+
+            objetotextoLogando.style.display = 'none';
+            objetoTextoLogin.style.display = 'inline';
         }
     });
 }
