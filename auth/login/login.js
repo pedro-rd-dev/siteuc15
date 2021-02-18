7d4ded916aaddd0b59cee50b1c2a1271e2a066ae
PEDRO <pedroh.mix@gmail.com>
2021-02-18 02:29:57 -0300

    Acertando login

--- auth/login/login.js
+++ auth/login/login.js
@@ -21,47 +21,9 @@ function login() {
         objetoAlertaEmail.innerHTML = ""
 
 
-        logar();
+        logarApi(objetoEmail.value,objetoPassword.value);
     }
 
 }
 
-function logar(){
-    var usuario = JSON.stringify({email:objetoEmail.value, password: objetoPassword.value})
 
-    $.ajax({
-        url: "http://escolarapp2.herokuapp.com/account/login/",
-        contentType: 'application/json',
-        cache: false,
-        method: 'POST',
-        dataType: 'json',
-        data: usuario,
-        success: function(objetoToken){
-            console.log(objetoToken)
-
-            /////////////////////ARMAZENAMENTO//////////////////////////
-            ///////////////////preparando token/////////////////////////
-            const key = "Token " + objetoToken.key;
-
-            //SALVANDO O TOKEN NO LOCAL STORAGE DO NAVEGADOR
-            window.localStorage.setItem("Token",key);
-
-
-            objetotextoLogando.style.display = 'none';
-            objetoTextoLogin.style.display = 'inline';
-
-            window.location.href ="../../tarefas/tarefas.html"
-
-        },
-        error: function (error){
-            var erros = JSON.parse(error.responseText)
-
-            console.log(erros.non_field_errors[0])
-
-            objetoAlertaEmail.innerHTML  =erros.non_field_errors[0]
-
-            objetotextoLogando.style.display = 'none';
-            objetoTextoLogin.style.display = 'inline';
-        }
-    });
-}
---
+++ auth/login/loginRequests.js
@@ -0,0 +1,45 @@
+function logarApi(email, password){
+    var usuario = JSON.stringify({email:email, password: password})
+
+    $.ajax({
+        url: "http://escolarapp2.herokuapp.com/account/login/",
+        contentType: 'application/json',
+        cache: false,
+        method: 'POST',
+        dataType: 'json',
+        data: usuario,
+        success: function(objetoToken){
+            console.log(objetoToken)
+
+            /////////////////////ARMAZENAMENTO//////////////////////////
+            ///////////////////preparando token/////////////////////////
+            const key = "Token " + objetoToken.key;
+
+            //SALVANDO O TOKEN NO LOCAL STORAGE DO NAVEGADOR
+            window.localStorage.setItem("Token",key);
+
+
+            objetotextoLogando.style.display = 'none';
+            objetoTextoLogin.style.display = 'inline';
+
+            window.location.href ="../../tarefas/tarefas.html"
+
+        },
+        error: function (error){
+            var erros = JSON.parse(error.responseText)
+
+            console.log(erros.non_field_errors[0])
+
+            objetoAlertaEmail.innerHTML  =erros.non_field_errors[0]
+
+            objetotextoLogando.style.display = 'none';
+            objetoTextoLogin.style.display = 'inline';
+        }
+    });
+}
+
+function  logoffApi(){
+    window.localStorage.removeItem("Token","");
+    console.log("indo para index")
+    window.location.href ="/index.html"
+}
